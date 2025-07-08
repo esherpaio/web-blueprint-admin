a89de66f68961db7507464506d2dfe6970056051
Stan Mertens <stnmertens@gmail.com>
2025-07-08 13:02:07 +0200

    change settings to config

--- web_bp_admin/routes/changelog.py
+++ web_bp_admin/routes/changelog.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import render_template
-from web.setup import settings
+from web.setup import config
 from web.utils.markdown import Markdown
 
 from web_bp_admin import admin_bp
@@ -9,7 +9,7 @@ from web_bp_admin import admin_bp
 
 @admin_bp.get("/admin/changelog")
 def changelog() -> str:
-    changelog_fp = os.path.join(settings.ROOT_DIR, "RELEASE.md")
+    changelog_fp = os.path.join(config.BASE_DIR, "RELEASE.md")
     with open(changelog_fp, "r") as file_:
         changelog_lines = file_.readlines()
     changelog_html = Markdown(*changelog_lines).html
