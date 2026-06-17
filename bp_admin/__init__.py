7f9aa9043e6c16b0a1dde87bbe1c73da2febf259
Stan Mertens <stnmertens@gmail.com>
2026-06-17 11:50:39 +0700

    repo alignment

--- bp_admin/__init__.py
+++ bp_admin/__init__.py
@@ -16,20 +16,3 @@ from .routes import (
     shipment_zones,
     users,
 )
-
-__all__ = [
-    "admin_bp",
-    "admin_static_jobs",
-    "categories",
-    "changelog",
-    "countries",
-    "coupons",
-    "emails",
-    "orders",
-    "products",
-    "settings",
-    "shipment_classes",
-    "shipment_methods",
-    "shipment_zones",
-    "users",
-]
